3b95019d2d7b14a9aaf8831aee22e69330713b44
Nathan Husemoller <43943527+husemoller@users.noreply.github.com>
2020-12-07 12:16:47 -0600

    Update UART_wrapper.sv
    Still need to update order of inputs/outputs

--- UART_wrapper.sv
+++ UART_wrapper.sv
@@ -1,85 +1,30 @@
-module UART_wrapper_SM(cmd_rdy, clr_rdy, sel, rx_rdy, clk, rst_n);
+module UART_wrapper(cmd, cmd_rdy, clr_cmd_rdy, RX, clk, rst_n);
 
-	output reg cmd_rdy, clr_rdy, sel;
-	input rx_rdy, clk, rst_n;
+	output [15:0] cmd;
+	output cmd_rdy;
+	input clr_cmd_rdy, RX, clk, rst_n;
 
-	reg rx_count;
-	logic start, received, set_rdy;
+	logic sel, rx_rdy, clr_rdy;
+	logic [7:0] rx_data, d, q;
 
-	///////////////////////////////
-	// Define state as enum type //
-	///////////////////////////////
-	typedef enum reg {IDLE, RECEIVE} state_t;
-	state_t state, next_state;
+	// Instantiate SM = State Machine //
+	UART_wrapper_SM iSM(.cmd_rdy(cmd_rdy), .clr_rdy(clr_rdy), .sel(sel), .clr_cmd_rdy(clr_cmd_rdy), .rx_rdy(rx_rdy), .clk(clk), .rst_n(rst_n));
 
-	//////////////////////
-	// Infer state flop //
-	//////////////////////
-	always_ff @(posedge clk, negedge rst_n) begin
-		if (!rst_n) begin
-	    		state <= IDLE;
-	  	end
-	  	else begin
-	    		state <= next_state;
-	    end
-	end
+	// Instantiate DP = Data Path //
+	UART_rcv iDP(.clk(clk), .rst_n(rst_n), .RX(RX), .rdy(rx_rdy), .rx_data(rx_data), .clr_rdy(clr_rdy));
 
-	/////////////////////////
-	// Infer rx_count flop //
-	/////////////////////////
-	always_ff @(posedge clk, negedge rst_n) begin
-		if (!rst_n || start) begin
-	    		rx_count <= 0;
-	  	end
-	  	else if (received) begin
-	  		rx_count <= 1;
-	  	end
-	  	else begin
-	    		rx_count <= rx_count;
-	    end
-	end
-
-	//////////////////////////////////
-	// Set sel based on state logic //
-	//////////////////////////////////
-    assign sel = received ? 1'b0 : 1'b1;	// Creates a 1 clk cycle delay so cmd_rdy is asserted at the same time as cmd is updated
-
-	/////////////////////////
-	// Infer cmd_rdy flop //
-	/////////////////////////
+	assign d[7:0] = sel ? rx_data[7:0] : q[7:0];
+	
+	//////////////////////////
+	// Infer cmd[15:8] flop //
+	//////////////////////////
 	always_ff @(posedge clk) begin
-		if (start) begin
-	    		cmd_rdy <= 0;
-	  	end
-	  	else if (set_rdy) begin
-	  		cmd_rdy <= 1;
-	  	end
-	  	else begin
-	    		cmd_rdy <= cmd_rdy;
-	    end
+		q <= d;
 	end
 
-	/////////////////////////
-	// State machine logic //
-	/////////////////////////
-	always_comb begin
-		start = 0;
-		received = 0;
-		set_rdy = 0;
+	//////////////////////////////////
+	// Set cmd based on flop output //
+	//////////////////////////////////
+	assign cmd[15:0] = {q[7:0], rx_data[7:0]};
 
-		case (state)
-			IDLE:		if (clr_rdy) begin
-							start = 1;
-							next_state = RECEIVE;
-						end
-			default: 	if (rx_rdy && rx_count == 1) begin
-							received = 1;
-							set_rdy = 1;
-							next_state = IDLE;	
-						end
-						else if (rx_rdy) begin
-							received = 1;
-						end
-		endcase
-	end
 endmodule
